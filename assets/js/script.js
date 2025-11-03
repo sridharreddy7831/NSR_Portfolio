0120f8e70a7a3a391b64be0b7e98e3c63b74cebf
Nalipi Sridhar Reddy <nalipisridharreddy@gmail.com>
2025-11-03 16:32:50 +0530

    Portfolio 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,189 +1,94 @@
-$(document).ready(function () {
+ // Function to set the theme and update UI
+ function setTheme(theme) {
+    document.body.setAttribute('data-bs-theme', theme);
+    localStorage.setItem('theme', theme);
+    var switchThemeBtn = document.getElementById('switchTheme');
+    if (switchThemeBtn) {
+        switchThemeBtn.innerHTML = theme === 'dark' ?  '<i class="bi bi-sun-fill"></i>' : '<i class="bi bi-moon-stars-fill"></i>';
+    }
+    //console.log(`Switched to ${theme} theme`);
+}
 
-    $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
+var currentTheme = localStorage.getItem('theme') || 'dark';
+setTheme(currentTheme);
+
+// Event listener for the switch theme button
+var switchThemeBtn = document.getElementById('switchTheme');
+if (switchThemeBtn) {
+    switchThemeBtn.addEventListener('click', () => {
+        currentTheme = currentTheme === 'dark' ? 'light' : 'dark';
+        setTheme(currentTheme);
     });
+}
 
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
+//AOS Initiliaze
+AOS.init();
 
-        if (window.scrollY > 60) {
-            document.querySelector('#scroll-top').classList.add('active');
-        } else {
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
+// Fixed Header & back to top button on Scroll
+window.addEventListener('scroll', () => {
+    // fixed header
+    const header = document.getElementById('header');
+    if (window.scrollY > 30 && !header.classList.contains('fixed-top')) {
+        header.classList.add('fixed-top');
+        document.getElementById('offcanvasNavbar').classList.add('fixedHeaderNavbar');
+    } else if (window.scrollY <= 30 && header.classList.contains('fixed-top')) {
+        header.classList.remove('fixed-top');
+        document.getElementById('offcanvasNavbar').classList.remove('fixedHeaderNavbar');
+    }
 
-        // scroll spy
-        $('section').each(function () {
-            let height = $(this).height();
-            let offset = $(this).offset().top - 200;
-            let top = $(window).scrollTop();
-            let id = $(this).attr('id');
+    //backtotop
+    const backToTopButton = document.getElementById("backToTopButton");
+    if (window.scrollY > 400 && backToTopButton.style.display === 'none') {
+        backToTopButton.style.display = 'block';
+    } else if (window.scrollY <= 400 && backToTopButton.style.display === 'block') {
+        backToTopButton.style.display = 'none';
+    }
+});
 
-            if (top > offset && top < offset + height) {
-                $('.navbar ul li a').removeClass('active');
-                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+
+//jumping to top function
+function scrollToTop(){
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+//Testimonial Slider
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:3,
+        nav:true,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive:{
+            0:{
+                items:1,
+            },
+            768:{
+                items:2,
+            },
+            1170:{
+                items:3,
             }
-        });
-    });
-
-    // smooth scrolling
-    $('a[href*="#"]').on('click', function (e) {
-        e.preventDefault();
-        $('html, body').animate({
-            scrollTop: $($(this).attr('href')).offset().top,
-        }, 500, 'linear')
-    });
-
-
-
-});
-
-document.addEventListener('visibilitychange',
-    function () {
-        if (document.visibilityState === "visible") {
-            document.title = "Portfolio | Sridhar Reddy Nalipi";
-            $("#favicon").attr("href", "./assets/images/NSR_LOGO.png");
-        }
-        else {
-            document.title = "Come Back To Portfolio";
-            $("#favicon").attr("href", "./assets/images/favhand.png");
         }
     });
-
-
-// <!-- typed js effect starts -->
-var typed = new Typed(".typing-text", {
-    strings: ["Final Year Student @ KSRMCE", "Froentend development", "Backend Developement", "Web Development", "Software Engineering"],
-    loop: true,
-    typeSpeed: 50,
-    backSpeed: 25,
-    backDelay: 500,
-});
-// <!-- typed js effect ends -->
-
-async function fetchData(type = "skills") {
-    let response
-    type === "skills" ?
-        response = await fetch("skills.json")
-        :
-        response = await fetch("./projects/projects.json")
-    const data = await response.json();
-    return data;
-}
-
-
-
-
-// <!-- tilt js effect starts -->
-VanillaTilt.init(document.querySelectorAll(".tilt"), {
-    max: 15,
-});
-// <!-- tilt js effect ends -->
-
-
-// pre loader start
-function loader() {
-        document.querySelector('.loader-container').classList.add('fade-out');
-        document.documentElement.classList.remove('no-scroll'); // Remove from html
-        document.body.classList.remove('no-scroll'); // Remove from body
-    }
-    
-    function fadeOut() {
-        setTimeout(loader, 3500); // Use setTimeout instead of setInterval for one-time execution
-    }
-    
-    window.onload = () => {
-        document.documentElement.classList.add('no-scroll'); // Add to html
-        document.body.classList.add('no-scroll'); // Add to body
-        fadeOut();
-    };
-    
-
-
-
-// pre loader end
-
-// disable developer mode
-document.onkeydown = function (e) {
-    if (e.keyCode == 123) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
-        return false;
-    }
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
-        return false;
-    }
-}
-
-
-
-
-/* ===== SCROLL REVEAL ANIMATION ===== */
-const srtop = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 1000,
-    reset: true
 });
 
-/* SCROLL HOME */
-srtop.reveal('.home .content h3', { delay: 200 });
-srtop.reveal('.home .content p', { delay: 200 });
-srtop.reveal('.home .content .btn', { delay: 200 });
 
-srtop.reveal('.home .image', { delay: 400 });
-srtop.reveal('.home .linkedin', { interval: 600 });
-srtop.reveal('.home .github', { interval: 800 });
-srtop.reveal('.home .twitter', { interval: 1000 });
-srtop.reveal('.home .telegram', { interval: 600 });
-srtop.reveal('.home .instagram', { interval: 600 });
-srtop.reveal('.home .dev', { interval: 600 });
+window.addEventListener('load', () => {
+  // Disable scroll when preloader is shown
+  document.body.classList.add('noscroll');
 
-/* SCROLL ABOUT */
-srtop.reveal('.about .content h3', { delay: 200 });
-srtop.reveal('.about .content .tag', { delay: 200 });
-srtop.reveal('.about .content p', { delay: 200 });
-srtop.reveal('.about .content .box-container', { delay: 200 });
-srtop.reveal('.about .content .resumebtn', { delay: 200 });
+  const preloader = document.getElementById('preloader');
 
+  setTimeout(() => {
+    preloader.classList.add('hide');
 
-/* SCROLL SKILLS */
-srtop.reveal('.skills .container', { interval: 200 });
-srtop.reveal('.skills .container .bar', { delay: 400 });
+    // Allow scroll after preloader is hidden
+    document.body.classList.remove('noscroll');
 
-/* SCROLL EDUCATION */
-srtop.reveal('.education .box', { interval: 200 });
-
-/* SCROLL PROJECTS */
-srtop.reveal('.work .box', { interval: 200 });
-
-/* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline', { delay: 400 });
-srtop.reveal('.experience .timeline .container', { interval: 400 });
-
-/* SCROLL CONTACT */
-srtop.reveal('.contact .container', { delay: 400 });
-srtop.reveal('.contact .container .form-group', { delay: 400 });
-
-
-
-// certifications
-
-
-
-
-
-// clock
-
-
-
+    setTimeout(() => {
+      preloader.style.display = 'none';
+    }, 100); // Match fade-out duration
+  }, 50); // 10 seconds // change this for preloader timer
+});
